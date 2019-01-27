bfa8047b75b0d4d5ef3f8142f2537681454f7176
Troy Sanders <troyjsanders@gmail.com>
2019-01-27 12:24:11 -0600

    Added some Event Entity Spawning magic

--- Assets/Scripts/EntitySpawner/EntitySpawnerEventBox.cs
+++ Assets/Scripts/EntitySpawner/EntitySpawnerEventBox.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class EntitySpawnerEventBox : EventBox, ISpawnParent
 {
     [SerializeField]
     private EntitySpawnerData m_entitySpawner;
+
+    [SerializeField]
+    private Transform[] m_spawnPoints;
 
     [SerializeField]
     private Transform m_waterTransform = null;
@@ -15,24 +19,31 @@ public class EntitySpawnerEventBox : EventBox, ISpawnParent
         // Do nothing
     }
 
+    private void Start()
+    {
+        
+    }
+
     public override void StartEvent()
     {
         // Randomly chooses from a predetermined list (chosen by a designer...so don't fuck it up )
 
-        if(m_entitySpawner == null)
+        if (m_entitySpawner == null)
         {
             Debug.LogErrorFormat("{0}: m_entitySpawner not assigned.", name);
             return;
         }
 
-        if(m_entitySpawner.entitiesToSpawn.Length <= 0)
+        if (m_entitySpawner.entitiesToSpawn.Length <= 0)
         {
             Debug.LogErrorFormat("{0}: No entities in the array entitiesToSpawn in m_entitySpawner. Exiting out of StartEvent()", name);
             return;
         }
 
+        m_spawnPoints = GetComponentsInChildren<Transform>();
+
         var entitiesToSpawn = m_entitySpawner.entitiesToSpawn;
-        m_entity = Instantiate(entitiesToSpawn[Random.Range(0, entitiesToSpawn.Length)], transform.position, Quaternion.identity);
+        m_entity = Instantiate(entitiesToSpawn[Random.Range(0, entitiesToSpawn.Length)], m_spawnPoints[Random.Range(0, m_spawnPoints.Length)].position, Quaternion.identity);
         var spawnedChild = m_entity.AddComponent<SpawnedChild>();
         spawnedChild.parent = this;
 
@@ -44,7 +55,7 @@ public class EntitySpawnerEventBox : EventBox, ISpawnParent
 
     public override void StopEvent()
     {
-        if(m_entity != null)
+        if (m_entity != null)
         {
             m_entity.SetActive(false);
         }
